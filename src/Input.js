68dcf3e14a94d745382ed77238be5af42aa4d3fc
AndrewThePug1 <andrewmondejarp@gmail.com>
2023-12-09 21:40:32 -0600

    Initial commit

---
+++ src/Input.js
@@ -0,0 +1,23 @@
+import colorNames from 'colornames'
+
+const Input = ( {colorValue, setColorValue, setHexValue }) => {
+    return (
+        <form onSubmit={(e) => e.preventDefault()}>
+            <label>Add Color name:</label>
+            <input
+                autoFocus
+                type="text"
+                placeholder="Add color name"
+                required 
+                value={colorValue}
+                onChange={(e) => { 
+                    setColorValue(e.target.value);
+                    setHexValue(colorNames(e.target.value));
+                }}
+
+                />
+        </form>
+    )
+}
+
+export default Input
